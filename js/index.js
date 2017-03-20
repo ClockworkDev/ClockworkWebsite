87037e7bafff37bd1788f678bc338c2d80451d5b
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-03-20 17:25:08 +0100

    Features

--- js/index.js
+++ js/index.js
@@ -83,4 +83,12 @@ window.addEventListener('scroll', function () {
     if (window.pageYOffset - window.innerHeight * 0.8 < -200) {
         document.getElementById("splashPage").classList.remove("fixedHeader");
     }
+
+    [].forEach.call(document.getElementsByClassName("feature"),function (x) {
+        if (x.getBoundingClientRect().top < window.innerHeight*0.8) {
+            x.classList.add("visibleFeature");
+        }else{
+            // x.classList.remove("visibleFeature");
+        }
+    });
 });
